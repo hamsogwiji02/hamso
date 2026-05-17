86f1c8b7aa1887610c2f0c6697b53b5e92593571
hamsogwiji02 <hamsogwiji02@gmail.com>
2026-05-17 17:48:44 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use("/api/auth", authRoutes);
 app.use(cors());
 app.use(express.json());
 
