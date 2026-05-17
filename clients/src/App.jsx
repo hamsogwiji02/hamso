22fc686a6cabbedbd20040bb99f430e66f71b7f0
hamsogwiji02 <hamsogwiji02@gmail.com>
2026-05-17 16:21:10 -0400

    Update App.jsx

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -11,7 +11,14 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+  path="/dashboard"
+  element={
+    <ProtectedRoute>
+      <Dashboard />
+    </ProtectedRoute>
+  }
+/>
     </Routes>
   );
 }
