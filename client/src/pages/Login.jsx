76c0228d2f75b77b26f9b38f611bd7ef13397b64
hamsogwiji02 <hamsogwiji02@gmail.com>
2026-05-27 11:26:51 -0400

    Create Login.jsx

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,25 @@
+export default function Login() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="glass p-10 rounded-2xl w-[350px]">
+        <h1 className="text-3xl font-bold mb-6">Login</h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full p-3 mb-4 rounded bg-slate-800"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-3 mb-4 rounded bg-slate-800"
+        />
+
+        <button className="bg-cyan-500 w-full py-3 rounded-xl">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
